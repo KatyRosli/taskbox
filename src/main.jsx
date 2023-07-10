f9791c61ed2dc112a8fd4ea91f65bc1365585780
Katy Rosli <katyrosli@gmail.com>
2023-07-10 17:55:05 +0200

    build out states

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ['../src/components/**/*.stories.@(js|jsx)'],
-   staticDirs: ['../public'],
-   addons: [
-     '@storybook/addon-links',
-     '@storybook/addon-essentials',
-     '@storybook/addon-interactions',
-   ],
-   framework: {
-     name: '@storybook/react-vite',
-     options: {},
-   },
-   docs: {
-     autodocs: 'tag',
-   },
- };
- export default config;
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
